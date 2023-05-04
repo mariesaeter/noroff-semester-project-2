905ed5f2bfdbcd943fc6d37ab7f662b51960873c
mariesaeter <marie.saeter@gmail.com>
2023-05-04 18:20:05 +0200

    Adds api and method constants

---
+++ src/js/tools/constants.mjs
@@ -0,0 +1,10 @@
+// Api urls
+export const api_Url_Base = "https://api.noroff.dev/api/v1/auction";
+export const api_Register = `${api_Url_Base}/auth/register`;
+export const api_Login = `${api_Url_Base}/auth/login`;
+export const api_Listings = `${api_Url_Base}/listings`;
+export const api_Profiles = `${api_Url_Base}/Profiles`;
+
+export const methodPost = "post";
+export const methodPut = "put";
+export const methodDelete = "delete";
