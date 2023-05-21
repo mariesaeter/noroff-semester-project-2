c42501322f296cf777cf6f26bb5ea66713187b29
mariesaeter <marie.saeter@gmail.com>
2023-05-21 21:22:06 +0200

    Adjusted login form validation

--- src/js/forms/loginForm.mjs
+++ src/js/forms/loginForm.mjs
@@ -1,13 +1,10 @@
 import { loginUser } from "../api/login.mjs";
-import { formValidation } from "../tools/formValidation.mjs";
 
 /**
  * Gatheres information from register form and calls function to try to register user
  */
 export function setLoginFormListener() {
   const form = document.querySelector("#loginForm");
-
-  formValidation();
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
